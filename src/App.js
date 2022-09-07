354f6b95540a37d1753b5761a379ba6211f9fe84
Gabriel <djabreu_@hotmail.com>
2022-09-07 18:01:48 -0400

    Se revierten algunos cambios que se utilizaron para la prueba de Fetch y useEffect

--- src/App.js
+++ src/App.js
@@ -1,44 +1,18 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-//import Card from "./components/Card";
-//import data from "./data";
-import React, { useState, useEffect } from "react";
+import Card from "./components/Card";
+import data from "./data";
 
 function App() {
-  const [tabl, setTabl] = useState([]);
-
-  useEffect(() => {
-    fetch("https://gorest.co.in/public/v2/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setTabl(data);
-      });
-  }, []);
-  const cards = tabl.map((item) => {
-    return (
-      <tr>
-        <td>{item.name}</td>
-        <td>{item.email}</td>
-        <td>{item.gender}</td>
-        <td>{item.status}</td>
-      </tr>
-    );
+  const cards = data.map((item) => {
+    return <Card id={item.id} item={item} />;
   });
   return (
     <div className='App'>
       <Navbar />
       <Hero />
-      <table>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-          <td>gender</td>
-          <td>status</td>
-        </tr>
-
-        {cards}
-      </table>
+      <section className='cards-list'>{cards}</section>
     </div>
   );
 }
