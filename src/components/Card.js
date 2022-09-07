35bea770a83fdc29f22a685a2b5262f8a0a7b2bd
Gabriel <djabreu_@hotmail.com>
2022-09-07 17:58:22 -0400

    * Se agregan las imagenes faltantes * Se agrega el mapeo y el archivo de datos [Data.js] * Se hace el mapeo de las props.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,34 +1,47 @@
 import React from "react";
 
+function Card(props) {
+  //Validacion para badge de sold out
+  //   let badgeText;
+  //   if (props.item.openSpots === 0) {
+  //     badgeText = "SOLD OUT";
+  //   } else if (props.item.location === "Online") {
+  //     badgeText = "ONLINE";
+  //   }
 
-function Card(props){
-    
-    //Validacion para badge de sold out
-    let badgeText
-    if (props.item.openSpots === 0) {
-        badgeText = "SOLD OUT"
-    } else if (props.item.location === "Online") {
-        badgeText = "ONLINE"
-    }
-
-return(
-    <div className="card">
-        {badgeText && <div className="card--badge"> {badgeText}</div>}
-    <img src={`./${props.item.coverImg}`} className="card--image" alt="card"/>
-    <div className="card--stats">
-        <img src="images/Star.png" className="card--star" alt="star"/>
-        <span>{props.item.stats.rating}</span>
-        <span className="gray">({props.item.statsreviewCount}) • </span>
-        <span className="gray">{props.item.location} </span>
+  return (
+    <div>
+      <table>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>gender</td>
+          <td>status</td>
+        </tr>
+        <tr>
+          <td>{props.item.name}</td>
+          <td>{props.item.email}</td>
+          <td>{props.item.gender}</td>
+          <td>{props.item.status}</td>
+        </tr>
+      </table>
     </div>
-    <p className="card--title">{props.item.title}</p>
-    <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
-</div>
-    );
+
+    //     <div className="card">
+    //         {badgeText && <div className="card--badge"> {badgeText}</div>}
+    //     <img src={`./${props.item.coverImg}`} className="card--image" alt="card"/>
+    //     <div className="card--stats">
+    //         <img src="images/Star.png" className="card--star" alt="star"/>
+    //         <span>{props.item.stats.rating}</span>
+    //         <span className="gray">({props.item.statsreviewCount}) • </span>
+    //         <span className="gray">{props.item.location} </span>
+    //     </div>
+    //     <p className="card--title">{props.item.title}</p>
+    //     <p className="card--price"><span className="bold">From ${props.item.price}</span> / person</p>
+    // </div>
+  );
 }
 
-
 export default Card;
-
 
 // `../images/${props.img}`
--- src/data.js
+++ src/data.js
@@ -1,68 +1,139 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default [
-    {
-        id: 1,
-        title: "Life Lessons with Katie Zaferes",
-        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
-        price: 136,
-        coverImg: "images/image12.png",
-        stats: {
-            rating: 5.0 ,
-            reviewCount: 6
-        },
-        location: "Online",
-        openSpots: 0,
-    },
-    {
-        id: 2,
-        title: "Learn Wedding Photography",
-        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
-        price: 125,
-        coverImg: "images/mountain-bike 1.png",
-        stats: {
-            rating: 5.0,
-            reviewCount: 30
-        },
-        location: "Online",
-        openSpots: 27,
-    },
-    {
-        id: 3,
-        title: "Group Mountain Biking",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "images/wed.png",
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Norway",
-        openSpots: 3,
-    },
-    {
-        id: 4,
-        title: "Group Mountain Biking",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "images/wed.png",
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Norway",
-        openSpots: 3,
-    },
-    {
-        id: 5,
-        title: "Group Mountain Biking",
-        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
-        price: 50,
-        coverImg: "images/wed.png",
-        stats: {
-            rating: 4.8,
-            reviewCount: 2
-        },
-        location: "Norway",
-        openSpots: 3,
-    }
-]
+  {
+    id: 3409,
+    name: "Ekalavya Nehru PhD",
+    email: "nehru_ekalavya_phd@pacocha.co",
+    gender: "male",
+    status: "inactive",
+  },
+  {
+    id: 3404,
+    name: "Dhruv Prajapat",
+    email: "dhruv_prajapat@koch.biz",
+    gender: "female",
+    status: "active",
+  },
+  {
+    id: 3399,
+    name: "Som Shukla",
+    email: "shukla_som@schiller.name",
+    gender: "male",
+    status: "inactive",
+  },
+  {
+    id: 3397,
+    name: "Kanaka Mehra",
+    email: "kanaka_mehra@waters.biz",
+    gender: "female",
+    status: "inactive",
+  },
+  {
+    id: 3396,
+    name: "Abhirath Dutta DC",
+    email: "dc_dutta_abhirath@armstrong.biz",
+    gender: "male",
+    status: "inactive",
+  },
+  {
+    id: 3394,
+    name: "Charumati Mishra",
+    email: "mishra_charumati@schaefer-cummerata.io",
+    gender: "female",
+    status: "inactive",
+  },
+  {
+    id: 3393,
+    name: "Anshula Jha",
+    email: "jha_anshula@carroll.co",
+    gender: "male",
+    status: "inactive",
+  },
+  {
+    id: 3392,
+    name: "Chanda Marar V",
+    email: "chanda_v_marar@spinka-boehm.co",
+    gender: "male",
+    status: "inactive",
+  },
+  {
+    id: 3391,
+    name: "Rep. Diptendu Jha",
+    email: "diptendu_jha_rep@rohan-harris.info",
+    gender: "male",
+    status: "active",
+  },
+  {
+    id: 3390,
+    name: "The Hon. Bhagvan Guneta",
+    email: "bhagvan_guneta_hon_the@larkin.biz",
+    gender: "female",
+    status: "active",
+  },
+];
+
+// {
+//     id: 1,
+//     title: "Life Lessons with Katie Zaferes",
+//     description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+//     price: 136,
+//     coverImg: "images/image12.png",
+//     stats: {
+//         rating: 5.0 ,
+//         reviewCount: 6
+//     },
+//     location: "Online",
+//     openSpots: 0,
+// },
+// {
+//     id: 2,
+//     title: "Learn Wedding Photography",
+//     description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+//     price: 125,
+//     coverImg: "images/mountain-bike 1.png",
+//     stats: {
+//         rating: 5.0,
+//         reviewCount: 30
+//     },
+//     location: "Online",
+//     openSpots: 27,
+// },
+// {
+//     id: 3,
+//     title: "Group Mountain Biking",
+//     description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+//     price: 50,
+//     coverImg: "images/wed.png",
+//     stats: {
+//         rating: 4.8,
+//         reviewCount: 2
+//     },
+//     location: "Norway",
+//     openSpots: 3,
+// },
+// {
+//     id: 4,
+//     title: "Group Mountain Biking",
+//     description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+//     price: 50,
+//     coverImg: "images/wed.png",
+//     stats: {
+//         rating: 4.8,
+//         reviewCount: 2
+//     },
+//     location: "Norway",
+//     openSpots: 3,
+// },
+// {
+//     id: 5,
+//     title: "Group Mountain Biking",
+//     description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+//     price: 50,
+//     coverImg: "images/wed.png",
+//     stats: {
+//         rating: 4.8,
+//         reviewCount: 2
+//     },
+//     location: "Norway",
+//     openSpots: 3,
+// }
