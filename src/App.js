adf4d491c786c7f5b21ddd4cafb0fbfc72f41d5f
Gabriel <djabreu_@hotmail.com>
2022-09-07 17:55:25 -0400

    * Se agregan las imagenes faltantes * Se agrega el mapeo y el archivo de datos [Data.js] * Se hace el mapeo de las props.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import Card from './components/Card';
-import data from './data'; 
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+//import Card from "./components/Card";
+//import data from "./data";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const cards= data.map((item) => {
-    return <Card
-    id={item.id}
-    item={item}
-    />
-  })
+  const [tabl, setTabl] = useState([]);
+
+  useEffect(() => {
+    fetch("https://gorest.co.in/public/v2/users")
+      .then((response) => response.json())
+      .then((data) => {
+        setTabl(data);
+      });
+  }, []);
+  const cards = tabl.map((item) => {
+    return (
+      <tr>
+        <td>{item.name}</td>
+        <td>{item.email}</td>
+        <td>{item.gender}</td>
+        <td>{item.status}</td>
+      </tr>
+    );
+  });
   return (
-    <div className="App">
-      <Navbar/>
-      <Hero/>
-   <section className='cards-list'>
-   {cards}
-    </section>
-    
-         </div>
+    <div className='App'>
+      <Navbar />
+      <Hero />
+      <table>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>gender</td>
+          <td>status</td>
+        </tr>
+
+        {cards}
+      </table>
+    </div>
   );
 }
 
